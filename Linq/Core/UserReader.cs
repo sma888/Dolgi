f3629842934c45554ff2ab0770d411f18fb57af6
sma888 <magamet.sitimov@gmail.com>
2022-11-13 19:33:21 +0300

    1

---
+++ Linq/Core/UserReader.cs
@@ -0,0 +1,38 @@
+﻿using Linq.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Linq.Core
+{
+    public class UserReader
+    {
+
+        public async Task<bool> UserWork(string Login, string Password)
+        {
+            using (StreamReader userReader = new StreamReader(@"..\..\File\User.txt"))
+            {
+                var userAwait = await userReader.ReadToEndAsync();
+                List<User> userList = new List<User>();
+
+                foreach (var item in userAwait.Split('\n'))
+                {
+                    var arrayString = item.Split('.');
+                    {
+                        var user = new User()
+                        {
+                            Login = arrayString[0],
+                            Password = arrayString[1],
+                        };
+                        if (user.Login.Contains(Login) && user.Password.Contains(Password))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Linq/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Linq.Core;
+using Linq.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Linq
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserReader reader = new UserReader();
+            var result = await reader.UserWork(loginTextBox.Text, passwordTextBox.Text);
+            if (loginTextBox.Text != "" && passwordTextBox.Text != "")
+            {
+                if (result == true)
+                {
+                    MessageBox.Show("Вход выполнен");
+                }
+                else MessageBox.Show("Неверный логин или пароль");
+            }
+            else MessageBox.Show("Не все поля заполнены");
+        }
+    }
+}
